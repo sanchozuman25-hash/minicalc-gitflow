5f1fc4713b39acef1afd016330cd332ba89a8a68
Manuel Sancho Zulueta <sancho.zuman25@triana.salesianos.edu>
2026-01-15 10:54:49 +0100

    feat: add subtract

--- MiniCalc.java
+++ MiniCalc.java
@@ -2,3 +2,7 @@ public class MiniCalc {
     public static int add(int a, int b) { return a + b; }
     // TODO: implement subtract
 }
+public static int subtract(int a, int b) { return a - b; }
+
+	}
+}
